a894537a5b98a0e040cb82c287d41e93e8c3dd79
nmt308 <minhthanh0946@gmail.com>
2023-01-16 18:44:16 +0700

    Netlify fix 404 not found

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const request = axios.create({
-    baseURL: 'https://ecommerce-sern-production.up.railway.app/api',
+    baseURL: 'https://nowshop-ecommerce.onrender.com/api',
 });
 export default request;
