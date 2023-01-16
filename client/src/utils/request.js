a6cdc8de7c65172a5def3afcb49bbcc9875c4838
nmt308 <minhthanh0946@gmail.com>
2023-01-16 13:46:58 +0700

    CORS option

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const request = axios.create({
-    baseURL: 'https://ecommerce-sern-production.up.railway.app/api',
+    baseURL: 'https://ecommerce-sern.vercel.app/api',
 });
 export default request;
